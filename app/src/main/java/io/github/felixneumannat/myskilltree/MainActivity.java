3379d86f00dab7f9b8d60819662ee69f0e1b0c21
Felix Neumann <147626082+FelixNeumannAT@users.noreply.github.com>
2025-08-13 09:51:16 +0200

    Set up project

---
+++ app/src/main/java/io/github/felixneumannat/myskilltree/MainActivity.java
@@ -0,0 +1,15 @@
+package io.github.felixneumannat.myskilltree;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toast.makeText(this, "Hello World", Toast.LENGTH_SHORT).show();
+    }
+}
